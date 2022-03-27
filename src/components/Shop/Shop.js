944812058864c830e14cd8b1f2a1a3b270c1564b
unknown <tareqrobin57@gmail.com>
2022-03-27 23:40:54 +0600

    shop style

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -21,8 +21,11 @@ const Shop = () => {
     const removeCart = () => {
         setCart([]);
     };
+
+    const selectedCart = Math.round(Math.random()*cart.length)%cart.length +1;
     return (
-        <div className='shop-container'>
+        <div className='shop-total'>
+            <div className='shop-container'>
             <div className="product-container">
                 {
                     products.map(product =><Product product={product} key={product.id} addToCart={addToCart}></Product>)
@@ -32,12 +35,14 @@ const Shop = () => {
                 <Carts cart={cart}></Carts>
 
                 <div>
-                <button className='choose-btn'>Choose 1 for me</button>
+                <button className='choose-btn' onClick={selectedCart}>Choose 1 for me</button>
 
                 <button className='choose-btn' onClick={removeCart}>Choose again</button>
             </div>
             </div>
-            <div className="questions">
+            
+        </div>
+        <div className="questions">
                 <div>
                     <h3>How React work?</h3>
                     <p>Components are made of two main things which are useState and props. These things mean Components can be organized in a hierarchical structure that confirms a one-way data flow (through props) so we only have one state. This structure is called a tree, and by this we can assign responsibility for a state to a Component.</p>
@@ -48,6 +53,7 @@ const Shop = () => {
                 </div>
             </div>
         </div>
+        
     );
 };
 
