8f94a14f64e9622b7b012c7b5e383da506f50a37
unknown <tareqrobin57@gmail.com>
2022-03-27 23:12:00 +0600

    answers given

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -37,7 +37,16 @@ const Shop = () => {
                 <button className='choose-btn' onClick={removeCart}>Choose again</button>
             </div>
             </div>
-            
+            <div className="questions">
+                <div>
+                    <h3>How React work?</h3>
+                    <p>Components are made of two main things which are useState and props. These things mean Components can be organized in a hierarchical structure that confirms a one-way data flow (through props) so we only have one state. This structure is called a tree, and by this we can assign responsibility for a state to a Component.</p>
+                </div>
+                <div>
+                    <h3>Difference between Props and State</h3>
+                    <p>We use Props to pass data from component to component. A state is a local data storage which is local to the component (only) and it cannot be passed to a different component.</p>
+                </div>
+            </div>
         </div>
     );
 };
